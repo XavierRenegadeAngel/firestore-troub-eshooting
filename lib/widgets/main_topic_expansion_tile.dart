3f3a289060a8a170081d3679db90098431a9f9bd
TJ <22714488+tjozsef@users.noreply.github.com>
2022-05-09 21:25:46 +0200

    Revert "Added custom widget as SubTopicExpansionTile. Refactored MainTopicExpansionTile to be stateless."
    This reverts commit 16386247b672e1fcb642f9073db586727ef133e5.

--- lib/widgets/main_topic_expansion_tile.dart
+++ lib/widgets/main_topic_expansion_tile.dart
@@ -2,37 +2,44 @@ import 'package:firestore_troubleshooting/database/database.dart';
 import 'package:firestore_troubleshooting/models/main_topic.dart';
 import 'package:firestore_troubleshooting/models/sub_topic.dart';
 import 'package:firestore_troubleshooting/widgets/add_topic_field_widget.dart';
-import 'package:firestore_troubleshooting/widgets/sub_topic_expansion_tile.dart';
 import 'package:flutter/material.dart';
 
-class MainTopicExpansionTile extends StatelessWidget {
+class MainTopicExpansionTile extends StatefulWidget {
   const MainTopicExpansionTile(
       {Key? key, required this.mainTopic, required this.database})
       : super(key: key);
   final MainTopic mainTopic;
   final Database database;
 
+  @override
+  State<MainTopicExpansionTile> createState() => _MainTopicExpansionTileState();
+}
 
+class _MainTopicExpansionTileState extends State<MainTopicExpansionTile> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dismissible(
       //Always add a uniq Key for dismissible, or it will not work well.
-      key: Key(mainTopic.docID),
+      key: Key(widget.mainTopic.docID),
       onDismissed: (direction) async {
-        await database.deleteMainTopic(mainTopic: mainTopic);
+        await widget.database.deleteMainTopic(mainTopic: widget.mainTopic);
       },
       child: ExpansionTile(
         leading: const Icon(Icons.view_headline_rounded),
         tilePadding: const EdgeInsets.all(8.0),
         childrenPadding: const EdgeInsets.only(left: 16.0),
         initiallyExpanded: false,
-        title: Text(mainTopic.name),
+        title: Text(widget.mainTopic.name),
         children: [
           AddTopicFieldWidget(
               onPressed: (subTopicName) {
-                database.setSubTopic(
-                  mainTopic: mainTopic,
+                widget.database.setSubTopic(
+                  mainTopic: widget.mainTopic,
                   subTopic: SubTopic(
                       name: subTopicName,
                       docID: DateTime.now().toIso8601String()),
@@ -41,7 +48,7 @@ class MainTopicExpansionTile extends StatelessWidget {
               buttonText: 'Add subtopic'),
           StreamBuilder<List<SubTopic>>(
             stream:
-                database.subTopicsStream(mainTopic: mainTopic),
+                widget.database.subTopicsStream(mainTopic: widget.mainTopic),
             initialData: const <SubTopic>[],
             builder: (context, snapshot) {
               if (snapshot.hasError) {
@@ -61,9 +68,17 @@ class MainTopicExpansionTile extends StatelessWidget {
                 itemCount: subTopicsList.length,
                 itemBuilder: (context, subTopicIndex) {
                   final subTopic = subTopicsList[subTopicIndex];
-                  return SubTopicExpansionTile(
-                    subTopic: subTopic,
-                    database: database,
+                  return ExpansionTile(
+                    initiallyExpanded: false,
+                    title: Text(subTopic.name),
+                    childrenPadding: const EdgeInsets.only(left: 16.0),
+                    children: [
+                      ListView(shrinkWrap: true, children: const [
+                        ListTile(
+                          title: Text('Teszt tile'),
+                        )
+                      ])
+                    ],
                   );
                 },
               );
